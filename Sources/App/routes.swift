b59c0fab81871fd7598a0c1d3481d1b49ade0319
xuanyuan <xuanyuanshenfu@outlook.com>
2023-06-02 16:17:08 +0800

    add

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -10,21 +10,20 @@ func routes(_ app: Application) throws {
     }
     
     app.get("ml",":v") { req -> AddUserModel in
-        let v = req.parameters.get("v") ?? ""
-        let data = Data(base64Encoded: v) ?? Data()
-        let decryptedString = String(data: data, encoding: .utf8) ?? ""
-        print(decryptedString)
-        let js = string2Json(text: decryptedString)
-        
-//        let originalString = "Hello, World!"
-        if let data1 = decryptedString.data(using: .utf8) {
-            let base64String = data1.base64EncodedString()
-            print(base64String)
+        DispatchQueue.global().sync {
+            let v = req.parameters.get("v") ?? ""
+            let data = Data(base64Encoded: v) ?? Data()
+            let decryptedString = String(data: data, encoding: .utf8) ?? ""
+            print(decryptedString)
+            let js = string2Json(text: decryptedString)
+            
+//            if let data1 = decryptedString.data(using: .utf8) {
+//                let base64String = data1.base64EncodedString()
+//                print(base64String)
+//            }
+            let num = predictFor(dic: js)
+            return AddUserModel(msg: "success", success: true,num: num)
         }
-        
-        
-        let num = predictFor(dic: js)
-        return AddUserModel(msg: "success", success: true,num: num)
     }
 
     
